d72113c75292f4260cb0ac6d72d8506f179eae14
MattFA24 <153161493+MattFA24@users.noreply.github.com>
2025-01-05 12:57:11 +0700

    route

--- src/routers/protected-router.ts
+++ src/routers/protected-router.ts
@@ -1,13 +1,13 @@
 import express from "express"
 // import { authMiddleware } from "../middlewares/auth-middleware"
 import { StudyController } from "../controllers/study-controller"
+import { SavedTranscriptController } from "../controllers/savedTranscript-controller"
 
 export const protectedRouter = express.Router()
 // protectedRouter.use(authMiddleware)
 
-// protectedRouter.delete("/api/logout", AuthController.logout)
-// protectedRouter.post("/api/todo", TodoController.createTodo)
-// protectedRouter.get("/api/todo", TodoController.getAllTodos)
-// protectedRouter.get("/api/todo/:todoId", TodoController.getTodo)
-// protectedRouter.put("/api/todo/:todoId", TodoController.updateTodo)
-// protectedRouter.delete("/api/todo/:todoId", TodoController.deleteTodo)
+protectedRouter.post("/api/savedTranscripts", SavedTranscriptController.create);
+protectedRouter.get("/api/savedTranscripts/:id", SavedTranscriptController.getById);
+protectedRouter.put("/api/savedTranscripts/:id", SavedTranscriptController.update);
+protectedRouter.delete("/api/savedTranscripts/:id", SavedTranscriptController.delete);
+protectedRouter.get("/api/users/savedTranscripts", SavedTranscriptController.getAllByUserId);
