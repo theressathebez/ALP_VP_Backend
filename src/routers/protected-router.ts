fb3cde3400d95f9a938a8772bee8eefe726a850f
theressathebez <153163348+theressathebez@users.noreply.github.com>
2025-01-05 18:36:05 +0700

    fix error dari merge

--- src/routers/protected-router.ts
+++ src/routers/protected-router.ts
@@ -8,21 +8,11 @@ export const protectedRouter = express.Router()
 protectedRouter.use(authMiddleware)
 
 protectedRouter.post("/api/logout", UserController.logout)
-// protectedRouter.delete("/api/logout", AuthController.logout)
 
 //user
 protectedRouter.post("/api/users/:userId/videos/:videoId", UserController.saveVideo);
 protectedRouter.get("/api/users/:userId/videos", UserController.getUserVideos)
 protectedRouter.delete("/api/users/:userId/videos/:videoId", UserController.deleteUserVideo);
-
-//study
-protectedRouter.post("/api/addTopic", StudyController.addTopic)
-protectedRouter.post("/api/addVideo", StudyController.createVideo)
-protectedRouter.get("/api/getTopic", StudyController.getTopics)
-protectedRouter.get("/api/getVideos/:topicId", StudyController.getVideosByTopic);
-protectedRouter.delete("/api/videos/:videoId", StudyController.deleteVideo);
-protectedRouter.put("/api/topics/:topicId", StudyController.updateTopic);
-protectedRouter.put("/api/videos/:videoId", StudyController.updateVideo);
 
 //transcript
 protectedRouter.post("/api/savedTranscripts", SavedTranscriptController.create);
--- src/routers/public-router.ts
+++ src/routers/public-router.ts
@@ -1,8 +1,17 @@
 import express from "express"
 import { UserController } from "../controllers/user-controller";
+import { StudyController } from "../controllers/study-controller";
 
 export const publicRouter = express.Router()
 
 publicRouter.post("/api/register", UserController.register)
 publicRouter.post("/api/login", UserController.login)
 
+//study
+publicRouter.post("/api/addTopic", StudyController.addTopic)
+publicRouter.post("/api/addVideo", StudyController.createVideo)
+publicRouter.get("/api/getTopic", StudyController.getTopics)
+publicRouter.get("/api/getVideos/:topicId", StudyController.getVideosByTopic);
+publicRouter.delete("/api/videos/:videoId", StudyController.deleteVideo);
+publicRouter.put("/api/topics/:topicId", StudyController.updateTopic);
+publicRouter.put("/api/videos/:videoId", StudyController.updateVideo);
